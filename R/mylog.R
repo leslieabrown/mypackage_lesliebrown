145c1864928b33131971749a214e872f5c938f21
lbrown <leslieabrown0@gmail.com>
2023-09-15 10:44:09 +0200

    Initial commit of mylog.R

---
+++ R/mylog.R
@@ -0,0 +1,19 @@
+#' Computing the log
+#'
+#' @param x a vector of length n
+#'
+#' @return a vector of length n with the log of x
+#' @export
+#'
+#' @examples
+#'
+#' v <- c(1,2,3)
+#' mylog(v)
+mylog <- function(x){
+  n <- length(x)
+  res <- numeric(n)
+  for(i in 1:n){
+    res[i] <- log(x[i])
+  }
+  return(res)
+}
